a8f4b20600d293fffc20fa6907609b0021c37370
liushuyu <liushuyu011@gmail.com>
2020-04-01 02:08:21 -0600

    ci: fix ci installation script

--- ci.sh
+++ ci.sh
@@ -3,4 +3,4 @@ HUGO_VERSION='0.68.3'
 
 echo 'Downloading Hugo...'
 wget -q "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_${HUGO_VERSION}_Linux-64bit.deb"
-dpkg -i "hugo_${HUGO_VERSION}_Linux-64bit.deb"
+sudo dpkg -i "hugo_${HUGO_VERSION}_Linux-64bit.deb"
