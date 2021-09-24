e3ecdb61a2516586f011af2f4aed4668caee458d
Kexy Biscuit <kexybiscuit@aosc.io>
2021-09-24 16:12:50 +0800

    ci.sh: update Hugo to 0.88.1

--- ci.sh
+++ ci.sh
@@ -1,5 +1,5 @@
 #!/bin/bash -e
-HUGO_VERSION='0.68.3'
+HUGO_VERSION='0.88.1'
 
 echo 'Downloading Hugo...'
 wget -q "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
