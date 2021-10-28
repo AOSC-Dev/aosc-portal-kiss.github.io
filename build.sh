8423c33c7cc672d1d10f828b9cb662ebae8d5f15
liushuyu <liushuyu011@gmail.com>
2021-10-28 00:18:29 -0600

    img/de-preview: use Hugo for image compression

--- build.sh
+++ build.sh
@@ -1,22 +1,6 @@
 #!/bin/bash -e
-TOPDIR=$(dirname "${BASH_SOURCE[0]}")
-# generate thumbnails
-function generateDEThumbnails() {
-    convert "$1" -resize 960x540 -quality 87 "$2"
-    echo "$1 -> $2"
-}
-
-echo 'Generating thumbnails...'
-
-for i in "${TOPDIR}/static/img/de-preview"/**/*.png; do
-    mkdir -p "$(dirname $i)/thumbs/"
-    generateDEThumbnails "${i}" "$(dirname $i)/thumbs/$(basename $i).jpg"
-done
-
-echo -e '\n... Done'
-
 echo 'Generating HTML files...'
 
-hugo --cleanDestinationDir
+hugo --cleanDestinationDir --gc
 
 echo '... Done'
