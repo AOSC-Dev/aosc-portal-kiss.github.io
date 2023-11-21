0613537e9669cde1445b85deaf1c70a28fe67f2d
liushuyu <liushuyu011@gmail.com>
2023-11-20 22:45:44 -0700

    CI: fix URL

--- ci.sh
+++ ci.sh
@@ -1,6 +1,6 @@
-#!/bin/bash -e
+#!/bin/bash -ex
 HUGO_VERSION='0.120.4'
 
 echo 'Downloading Hugo...'
-wget -q "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
-sudo dpkg -i "hugo_extended_${HUGO_VERSION}_Linux-64bit.deb"
+wget -q "https://github.com/gohugoio/hugo/releases/download/v${HUGO_VERSION}/hugo_extended_${HUGO_VERSION}_linux-amd64.deb"
+sudo dpkg -i "hugo_extended_${HUGO_VERSION}_linux-amd64.deb"
